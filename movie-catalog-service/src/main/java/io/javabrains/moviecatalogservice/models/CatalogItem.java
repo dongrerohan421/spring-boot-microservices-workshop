18514501fd01c5e8d29235a844c62e90aeccd7d1
Rohan Dongre <rdongre@ford.com>
2019-03-04 20:06:33 -0500

    02.Added Movie Catalog API

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,16 @@
+package io.javabrains.moviecatalogservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CatalogItem {
+    private String name;
+    private String desc;
+    private int rating;
+}
